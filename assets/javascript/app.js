e32a8e6c9bb9f7aeb8f044fc888f3182c69543ca
Katy <ihatetoast@gmail.com>
2017-12-11 20:19:05 -0600

    show hide divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,26 @@
+$(document).ready(function(){
+  //DOM PLAY
+  $("#restrictionsDiv").hide();
+  $("#ingredientsDiv").hide();
 
+  $("#restrictionsIntro").on("click", function(){
+    $(this).hide();
+    $("#pantryIntro").hide();
+    $("#restrictionsDiv").show();
+  });
+  $("#pantryIntro").on("click", function(){
+    $(this).hide();
+    $("#restrictionsIntro").hide();
+    $("#ingredientsDiv").show();
+  });
+
+  $(".home-btn").on("click", goHome);
+
+  function goHome(){
+    $("#restrictionsDiv").hide();
+    $("#ingredientsDiv").hide();
+    $("#pantryIntro").show();
+    $("#restrictionsIntro").show();
+  }
+  
+  });
