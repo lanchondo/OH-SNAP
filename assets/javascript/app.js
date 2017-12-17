e598c6449a619e516ee4188a39cfe94c46c20d50
Joshua <Joshua@Admins-MacBook.local>
2017-12-17 01:03:18 -0600

    adding autofill and modified create card list to only add card ingredients

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,30 @@
+
+
+var input = document.getElementById("basics");
+var awesomeplete = new Awesomplete(input);
+
+
+function recipeapicallback(fillData) {
+    awesomeplete.list = fillData;
+}
+
+$('#basics').on('input', function() {
+     var inString = $(this).val();
+    getAutocomplete(inString);
+});
+
+
+function getAutocomplete(inString){
+    //api needs callback in order to provide return 
+    //datatype needs to be set to jsonp
+    var queryURL = `http://api.edamam.com/auto-complete?q=${inString}&limit=10&app_id=cb021850&app_key=3f0c4b8c9adcb08d63cbde97230db9f8&callback=recipeapicallback`
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: 'jsonp'
+      });
+    
+};
 //click on food restriction icon and triggers api 
 //also has array of food ingredients for food search and food restriction diet
    $('.food-icon-restr').on('click', function() {
@@ -7,9 +34,9 @@
         var ingredients=["chicken","onion","apple"];
         runRestrictedDietAPI(restrictDietChoice, ingredients);
         //items you have in stock to store in firebase
-        var pantry = ["garlic", "tomato", "potato", "beef"]
+        //var pantry = ["garlic", "tomato", "potato", "beef"]
         //adds pantry items to persisting source
-        pantryStorage(pantry);
+        //pantryStorage(pantry);
     });
 
 //runs retricted diet recipe search API
@@ -19,22 +46,18 @@
         $.each(ingredients, function(index,value){
             strIngredient += value +"%20";
         })
-        console.log(strIngredient);
-        var queryURL = `https://api.edamam.com/search?q=${strIngredient}&health=${restrictDietChoice}&app_id=2e4ce701&app_key=20b246a2d182f864dd85c155afc277d3`
+
+        var queryURL = `https://api.edamam.com/search?q=${strIngredient}&health=${restrictDietChoice}&app_id=2e4ce701&app_key=20b246a2d182f864dd85c155afc277d3`;
         $.ajax({
             url: queryURL,
             method: "GET"
           }).done(function(response) {
-            console.log(response);
             createRecipeCards(response);
-        //trying to create paging for going to previous 10 or next 10 results
-        //not adding functionality currently
-            // $('.card-text').on('click', function() {
-            //     pageResults(this.id,response);
-            // });
           });
+        
           
     }
+
     //changes results
      //not adding functionality currently
         // function pageResults(id,response){
@@ -51,23 +74,26 @@
         //         }
         //     } 
         // }
-
+       
+        
         function createRecipeCards(response){
+            $(".card-group").empty();
             $.each(response.hits, function(index, value){
+                var hits= index;
                 $(".card-group").append(`
                 <div class="card border-primary mb-3" style="max-width: 20rem;">
                 <div class="card-header">${value.recipe.label}</div>
                 <div class="card-body text-primary">
-                <img src="${value.recipe.image}" alt="${value.recipe.label}" height="42" width="42">
-                  <h2 class="card-title">Total Calories:${parseInt(value.recipe.calories)}</h2>
-                  <h3 class="card-title">Individual Serving Calories:${parseInt(value.recipe.calories/value.recipe.yield)}</h3>
-                  <ul class="ingr-list"></ul> 
-                  <a href="${value.recipe.url}">Diretions to make this</a>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                  `)
+                    <img src="${value.recipe.image}" alt="${value.recipe.label}" height="42" width="42">
+                    <h2 class="card-title">Total Calories:${parseInt(value.recipe.calories)}</h2>
+                    <h3 class="card-title">Individual Serving Calories:${parseInt(value.recipe.calories/value.recipe.yield)}</h3>
+                    <ul class="ingr-list" id="${index}"></ul> 
+                    <a href="${value.recipe.url}">Diretions to make this</a>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                `)
                   $.each(value.recipe.ingredients, function(index, value){
-                    $('.ingr-list').append(`<li>${value.text}</li>`)  
+                    $('#'+hits).append(`<li>${value.text}</li>`)  
                   })
             });
         }
