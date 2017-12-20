02b40ac66e5383ed5a83c12d665b259aff193b18
Lauryn McFarland <lauryn@Lauryns-MacBook-Pro.local>
2017-12-19 19:38:24 -0600

    added ability to toggle search on and off

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 // STYLE RELATED F(X):
 $(function() 
-  {
-    $("#circleBtn").mouseenter(function(e) {
-        $(this).addClass("animated pulse");
-        
-    });
-    $("#circleBtn").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(e) {
-        $(this).removeClass("animated pulse");
-    });
+{
+  $("#circleBtn").mouseenter(function(e) {
+    $(this).addClass("animated pulse");
+
   });
+  $("#circleBtn").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(e) {
+    $(this).removeClass("animated pulse");
+  });
+});
 
 //click on food restriction icon and triggers api 
 //also has array of food ingredients for food search and food restriction diet
@@ -17,141 +17,155 @@ $(function()
 var ingredients=[];
 
 $('.food-icon-restr').on('click', function() {
-   var restrictDietChoice = this.id;
-   runRestrictedDietAPI(restrictDietChoice, ingredients);
+ var restrictDietChoice = this.id;
+ runRestrictedDietAPI(restrictDietChoice, ingredients);
 });
 
 //runs restricted diet recipe search API
 function runRestrictedDietAPI(restrictDietChoice, ingredients){
-   var strIngredient = "";
+ var strIngredient = "";
    //concats array with wildcard for better api search
    $.each(ingredients, function(index,value){
-       strIngredient += value +"%20";
+     strIngredient += value +"%20";
    })
    var queryURL = `https://api.edamam.com/search?q=${strIngredient}&health=${restrictDietChoice}&app_id=2e4ce701&app_key=20b246a2d182f864dd85c155afc277d3`
    $.ajax({
-       url: queryURL,
-       method: "GET"
-     }).done(function(response) {
-       createRecipeCards(response);
-     });    
-};
+     url: queryURL,
+     method: "GET"
+   }).done(function(response) {
+     createRecipeCards(response);
+   });    
+ };
 
-   function createRecipeCards(response){
+ function createRecipeCards(response){
     // $(".cardGroup").empty();
-       $.each(response.hits, function(index, value){
-        var hits = index;
-          $(".cardGroup").append(`
-            <div class="recipeCard">
-              <div class="imgDiv">
-                <img src="${value.recipe.image}" alt="${value.recipe.label}">
-              </div>
-              <div class="recipeLabel">
-                <h5>${value.recipe.label}</h5>
-              </div>
-              <p class = "cals">Individual Serving Calories:${parseInt(value.recipe.calories/value.recipe.yield)}</p>
-              <ul id="${index}">
-              </ul>
-              <button class="recipeBtn"><a href="${value.recipe.url}">Link to source</a></button>
-              <button class="nutBtn"><a href="#">Nutritional Value</a></button>  
-            </div>
-             `)
-            $.each(value.recipe.ingredients, function(index, value){
-              $('#'+hits).append(`<li>${value.text}</li>`)  
-            })
-       });   
-   }
+    $.each(response.hits, function(index, value){
+      var hits = index;
+      $(".cardGroup").append(`
+        <div class="recipeCard">
+        <div class="imgDiv">
+        <img src="${value.recipe.image}" alt="${value.recipe.label}">
+        </div>
+        <div class="recipeLabel">
+        <h5>${value.recipe.label}</h5>
+        </div>
+        <p class = "cals">Individual Serving Calories:${parseInt(value.recipe.calories/value.recipe.yield)}</p>
+        <ul id="${index}">
+        </ul>
+        <button class="recipeBtn"><a href="${value.recipe.url}">Link to source</a></button>
+        <button class="nutBtn"><a href="#">Nutritional Value</a></button>  
+        </div>
+        `)
+      $.each(value.recipe.ingredients, function(index, value){
+        $('#'+hits).append(`<li>${value.text}</li>`)  
+      })
+    });   
+  }
 
 
- 
-var pantry = [];
+
+  var pantry = [];
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBk2pHNz0EinDznYAMc6g_quxiE5uByHzQ",
-    authDomain: "fir-proj-fc54a.firebaseapp.com",
-    databaseURL: "https://fir-proj-fc54a.firebaseio.com",
-    projectId: "fir-proj-fc54a",
-    storageBucket: "fir-proj-fc54a.appspot.com",
-    messagingSenderId: "936997790614"
+  apiKey: "AIzaSyBk2pHNz0EinDznYAMc6g_quxiE5uByHzQ",
+  authDomain: "fir-proj-fc54a.firebaseapp.com",
+  databaseURL: "https://fir-proj-fc54a.firebaseio.com",
+  projectId: "fir-proj-fc54a",
+  storageBucket: "fir-proj-fc54a.appspot.com",
+  messagingSenderId: "936997790614"
 };
 firebase.initializeApp(config);
 var database = firebase.database();
 
 function pantryStorage(pantry){
-    $.each(pantry, function(index,value){
-        var pantryItem = {
-            item: value,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        }
-        database.ref("Pantry").push(pantryItem);
-    });
-    $("#temporaryPantry").empty();
-    console.log('temp pantry emptied')
+  $.each(pantry, function(index,value){
+    var pantryItem = {
+      item: value,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    }
+    database.ref("Pantry").push(pantryItem);
+  });
+  $("#temporaryPantry").empty();
+  console.log('temp pantry emptied')
 };
 
 $("#pantry-input").on("click", function(event) {
   event.preventDefault();
       // console.log($("#pantry-add").val().trim());
-    $("#temporaryPantry").empty();
+      $("#temporaryPantry").empty();
       pantry.push($("#basics").val().trim());
       console.log(pantry);
       console.log("added to pantry");
       $.each(pantry, function(index,value){
         $("#temporaryPantry").append(`<li><i class="fa fa-cutlery" aria-hidden="true"></i> ${value}</li>`);
-    });
+      });
       //add something tp clear out previour text
-  });
+    });
 
 $("#pantry-submit").on("click", function(event) {
-    console.log("submitting pantry...");
-    pantryStorage(pantry);
+  console.log("submitting pantry...");
+  pantryStorage(pantry);
 });
 
 database.ref("Pantry").on("child_added", function(childSnapshot) {
-    var expirationDT = moment().subtract(7, 'days').format('YYYY/MM/DD HH:mm:ss');
-    var item = childSnapshot.val().item;
-    var key = childSnapshot.key;
-    if(moment(childSnapshot.val().dateAdded).format('YYYY/MM/DD HH:mm:ss') < expirationDT){
-        $(".pantry-current").append(`<span class="pantry-item spoiled" id="${key}""><a href='javascript:void(0);' class='remove'>&times;</a>${item}<a href='javascript:void(0);' class='add'>&#10010;</a><span>`);
-    }else{
-        $(".pantry-current").append(`<span class="pantry-item" id="${key}""><a href='javascript:void(0);' class='remove'>&times;</a>${item}<a href='javascript:void(0);' class='add'>&#10010;</a><span>`);
-    }
+  var expirationDT = moment().subtract(7, 'days').format('YYYY/MM/DD HH:mm:ss');
+  var item = childSnapshot.val().item;
+  var key = childSnapshot.key;
+  if(moment(childSnapshot.val().dateAdded).format('YYYY/MM/DD HH:mm:ss') < expirationDT){
+    $(".pantry-current").append(`<span class="pantry-item spoiled" id="${key}""><a href='javascript:void(0);' class='remove'>&times;</a>${item}<a href='javascript:void(0);' class='add'>&#10010;</a><span>`);
+  }else{
+    $(".pantry-current").append(`<span class="pantry-item" id="${key}""><a href='javascript:void(0);' class='remove'>&times;</a>${item}<a href='javascript:void(0);' class='add'>&#10010;</a><span>`);
+  }
 });
 
 $(document).ready(function(){
-    $(document).on("click", "a.remove" , function() {
-        $(this).parent().remove();
-        removeID = $(this).parent().attr("id");
-        database.ref("Pantry/" + removeID).remove();
-        console.log(`Removed ID: ${removeID}`);
-    });
+  //remove pantry items from firebase database
+  $(document).on("click", "a.remove" , function() {
+    $(this).parent().remove();
+    removeID = $(this).parent().attr("id");
+    database.ref("Pantry/" + removeID).remove();
+    console.log(`Removed ID: ${removeID}`);
+  });
+    //add pantry items to ingredient search list
     $(document).on("click", "a.add" , function() {
+      var item = $(this).parent().text();
+      //remove the special characters from the beginning and end of the ingredient
+      item = item.slice(0, -1);
+      item = item.substr(1);
+      console.log($(this).attr("id"));
+      if ($(this).attr("id") == "added") {
+        console.log("Item removed: " + item);
+        $(this).parent().css('background-color', 'transparent');
+        var i = ingredients.indexOf(item);
+        ingredients.splice(i, 1);//remove the clicked item fron the ingreidients search
+        $(this).attr("id", "removed")
+        console.log(ingredients);
+      } else {
+        console.log("Item added: " + item);
         $(this).parent().css('background-color', 'gray');
-        var item = $(this).parent().text();
-        //remove the special characters from the beginning and end of the ingredient
-        item = item.slice(0, -1);
-        item = item.substr(1);
-        console.log(item);
         ingredients.push(item);
+        console.log(ingredients);
+        $(this).attr("id", "added")
+      }
     });
-});
+  });
 
 var input = document.getElementById("basics");
 var awesomeplete = new Awesomplete(input);
 function recipeapicallback(fillData) {
-    awesomeplete.list = fillData;
+  awesomeplete.list = fillData;
 }
 var inString;
 $('#basics').on('input', function() {
-     inString = $(this).val();
-    getAutocomplete(inString);
+ inString = $(this).val();
+ getAutocomplete(inString);
 });
 function getAutocomplete(inString){
-    var queryURL = `http://api.edamam.com/auto-complete?q=${inString}&limit=10&app_id=cb021850&app_key=3f0c4b8c9adcb08d63cbde97230db9f8&callback=recipeapicallback`
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        dataType: 'jsonp'
-      });
-    
+  var queryURL = `http://api.edamam.com/auto-complete?q=${inString}&limit=10&app_id=cb021850&app_key=3f0c4b8c9adcb08d63cbde97230db9f8&callback=recipeapicallback`
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    dataType: 'jsonp'
+  });
+
 };
